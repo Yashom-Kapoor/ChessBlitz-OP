d5099f8883186615cda27095a2c1d0ce6ece1eeb
LeoJPark <leopark@berkeley.edu>
2025-11-03 19:50:36 -0800

    Update files

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -330,7 +330,8 @@ def update_rating():
         return jsonify({"error": f"Failed to update rating: {str(e)}"}), 500
 
 def main():
-    app.run(debug=False, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+    #app.run(debug=False, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
